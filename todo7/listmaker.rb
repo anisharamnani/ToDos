de9eb76acef1c543f15a26074e5990013263d84d
anisharamnani <anisha.ramnani@gmail.com>
2013-11-20 10:48:20 -0500

    refactored todo7

--- todo7/listmaker.rb
+++ todo7/listmaker.rb
@@ -9,12 +9,24 @@
 # e.g make_list(["ich", "ni", "san"]) #=> ["1. ich", "2. ni", "3. san"]
 
 def make_list array
-	num = 1
-	list_array = []
-	array.each do |value|
-	# return an array with an added number referrring to it's place in the index + 1 
-		list_array << num.to_s + "." + " " + value
-		num = num + 1  
-	end 
-	list_array
+	array.map.with_index{|item, index| "#{index+1}. #{item}"}
 end 
+
+# def make_list
+# 	x = []
+# 	array.each_with_index do |x, index|
+# 		list_array << "{index+1}. #{x}"
+# 	end 
+# end 
+
+# def make_list array
+# 	num = 1
+# 	list_array = []
+# 	array.each do |value|
+# 	# return an array with an added number referrring to it's place in the index + 1 
+# 		list_array << "#{num}. #{value}"
+# 		num += 1
+# 	end 
+# 	list_array
+# end 
+
