07d76bf639b8574a07e7b5124735b5e2fcdfbae1
anisharamnani <anisha.ramnani@gmail.com>
2013-12-05 10:51:40 -0500

    Triangle Error works

--- todo14/triangle.rb
+++ todo14/triangle.rb
@@ -7,14 +7,21 @@ class Triangle
 		@c = c
 	end 
 
-	def kind 
-
+	def kind
+		raise TriangleError if not triangle? 
 		return :equilateral if a == b && b == c  
 		return :isosceles if (a == b && a != c) || (b == c && b!= a) || (a == c && a != b)
 		return :scalene if (a != b) && (b != c) && (a != c)
 	end 
 
+	def triangle?
+		return false if a <= 0 || b <= 0 || c <= 0 
+
+		return true if (a+b)>c && (b+c)>a && (c+a)>b
+	end 
+
 end 
 
-class TriangleError < StandardError 
+class TriangleError < StandardError
+	"NEIN. NOT A TRIANGLE." 
 end 
