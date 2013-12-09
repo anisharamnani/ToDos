41a86acc6a0861baf823eb4a0f200090750100b3
anisharamnani <anisha.ramnani@gmail.com>
2013-12-09 09:23:57 -0500

    todo16.rb

---
+++ todo16/todo16.rb
@@ -0,0 +1,16 @@
+# Instructions:
+# Count elements in an Array by returning a hash with keys of the elements and values of the amount of times they occur.
+
+test = ['cat', 'dog', 'fish', 'fish']
+
+def count(array)
+	new_test = {}
+	array.uniq!
+	array.each do |item|
+		new_test[item] = array.index(item) + 1
+	end 
+	puts new_test
+end
+
+count(test) #=> { 'cat' => 1, 'dog' => 1, 'fish' => 2 })
+
