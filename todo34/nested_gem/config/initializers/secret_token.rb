6ba66588f265df281738fa11d2ef506241afec8a
anisharamnani <anisha.ramnani@gmail.com>
2014-01-13 09:29:54 -0500

    gem to auto generate nested: not working or twerking

---
+++ todo34/nested_gem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NestedGem::Application.config.secret_token = '2d6f59f9cd9b0e566c6afe41c60536ba783f20f0002683d09456a71965a8ee9d9a9dbb0494f988ad8c240b7aebfee342d54b37e2d91758447fbe74f834576028'
